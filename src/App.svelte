ee8ebc4d63ffbb4091aaf3b2059dd428fd69bb34
Harvey Nwynn <harveynwynn@gmail.com>
2022-01-30 20:31:07 -0800

    todo list core

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
-	export let name: string;
+import { prevent_default } from "svelte/internal";
+
+	let names: string[] = ['Harvey', 'Bao', 'Michael'];
+	let name: string;
+	const handleOnSubsmit = () => {
+		names = [...names, name];
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+		<input bind:value={name}>
+		<button on:click={handleOnSubsmit}>Submit</button>
+	<ul>
+		{#each names as name}
+			<li>{name}</li>
+		{/each}
+	</ul>
 
+</main>
 <style>
 	main {
 		text-align: center;
